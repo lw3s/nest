541ed52600c970aec1bb66da60e1ca256a7b429f
Lucas Grenier <luc@sgrenier.com>
2024-12-30 01:59:03 -0500

    Remove ostream test
    The Edit test kills 2 birds, and my implementation was stupid anyway.

--- tests/list.cpp
+++ tests/list.cpp
@@ -20,18 +20,6 @@ public:
     }
 };
 
-TEST_F(LinkedListTest, OStream) {
-    std::system("touch ostream_test.txt");
-    std::fstream fp("ostream_test.txt", std::fstream::in | std::fstream::app);
-    ASSERT_TRUE(fp.is_open());
-    fp << ls;
-    fp.seekg(0);
-    std::string text_version(std::istreambuf_iterator<char>(fp), (std::istreambuf_iterator<char>()));
-    EXPECT_EQ(text_version, "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}");
-    fp.close();
-    std::system("rm ostream_test.txt");
-}
-
 TEST_F(LinkedListTest, Conversion) {
     int static_ints[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     List<int> from_static = List(static_ints, 20);
