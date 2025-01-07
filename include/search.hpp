053f95228c119195fa1cd6095ab3a511e08773fe
Lucas Grenier <luc@sgrenier.com>
2025-01-07 13:41:17 -0500

    Implement linear and binary

--- include/search.hpp
+++ include/search.hpp
@@ -1,14 +1,27 @@
 #include <vector>
 #include <cstdlib>
+#include <stdexcept>
 
 namespace search {
 
 size_t linear(std::vector<int> arr, int val) {
-    return 0; // prevent compile warnings for now
+    for (size_t i = 0; i < arr.size(); ++i) {
+        if (arr[i] == val) return i;
+    }
+    throw std::range_error("arr does not contain val");
 }
 
 size_t binary(std::vector<int> arr, int val) {
-    return 0;
+    size_t l = 0, r = arr.size() - 1, m;
+    if (arr[l] == val) return l;
+    if (arr[r] == val) return r;
+    while (r - l >= 2) {
+        m = size_t((l + r) / 2);
+        if (val == arr[m]) return m;
+        if (val < arr[m]) r = m;
+        if (val > arr[m]) l = m;
+    }
+    throw std::range_error("arr is unsorted or does not countain val");
 }
 
 size_t bintree(std::vector<int> arr, int val) { // see docs
--- testing/search.cpp
+++ testing/search.cpp
@@ -5,14 +5,16 @@
 
 class SearchTest : public testing::Test {
 protected:
-    std::vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 };
 
 TEST_F(SearchTest, Binary) {
-
+    EXPECT_EQ(search::binary(arr, 10), 9);
+    EXPECT_EQ(search::binary(arr, 2), 1);
+    EXPECT_EQ(search::binary(arr, 19), 18);
 }
 
-TEST_F(SearchTest, Bintree) {
+TEST_F(SearchTest, Bintree)  {
 
 }
 
