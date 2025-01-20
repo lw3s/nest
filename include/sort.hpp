3eed54e839dadd2ab34e10b2f4beb387ee6fb35e
Drew J Blome <drewjblome@Drews-MacBook-Pro.local>
2025-01-20 17:52:48 -0500

    Final project

--- include/sort.hpp
+++ include/sort.hpp
@@ -36,6 +36,42 @@ int partition(std::vector<int>& arr, int low, int high) { // For QUICK ---------
     std::swap(arr[i + 1], arr[high]);
     return i + 1;
 }
+void mergeHelp(std::vector<int>& arr, int left, int mid, int right) { // For MERGE ---
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> leftArr(n1), rightArr(n2);
+
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
 
 
 
@@ -78,15 +114,19 @@ void quick(std::vector<int>& arr, int low = 0, int high = -1) {
     }
 }
    
-void merge(std::vector<int>& arr) {
-    //if (left < right) {
-        //int mid = left + (right - left) / 2;
+void merge(std::vector<int>& arr, int left = 0, int right = -1) {
+    if (right == -1) {
+        right = arr.size() - 1;
+    }
+    
+    if (left < right) {
+        int mid = left + (right - left) / 2;
 
-        //merge(arr, left, mid);
-       // merge(arr, mid + 1, right);
+        merge(arr, left, mid);
+        merge(arr, mid + 1, right);
 
-        //mergeHelp(arr, left, mid, right);
-    //}
+        mergeHelp(arr, left, mid, right);
+    }
 }
 
 }
