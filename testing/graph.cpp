c0873b05eb130781d596bab7999344d326d2217d
Lucas Grenier <luc@sgrenier.com>
2025-01-25 01:04:26 -0500

    Set back Graph mistakes

--- testing/graph.cpp
+++ testing/graph.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
+
 #include "graph.hpp"
 
-std::vector<int> sort(std::vector<int> arr) { // quick-and-dirty selection sort; will change once we get sorting algorithms implemented
+
+std::vector<int> sort(std::vector<int> arr) { // quick-and-dirty selection sort
     for (int i = 0; i < arr.size(); ++i) {
         for (int j = i; j < arr.size(); ++j) {
             if (arr[j] < arr[i]) {
@@ -15,17 +17,16 @@ std::vector<int> sort(std::vector<int> arr) { // quick-and-dirty selection sort;
     return arr;
 }
 
+
 TEST(GraphTest, InsertionAndDeletion) {
     Graph<int> g;
-    g.clear_node(1);
-    EXPECT_THROW(g.update_edge(1, 2, 5);, NonexistentNode);
-    g.clear_node(2);
+    g.add_node(1);
+    EXPECT_THROW(g.set_edge(1, 2, 5);, NonexistentNode);
+    g.add_node(2);
     EXPECT_THROW(g.weight(1, 2);, NonexistentEdge);
-    g.update_edge(1, 2, 5);
+    g.set_edge(1, 2, 5);
     EXPECT_DOUBLE_EQ(g.weight(1, 2), 5);
     EXPECT_EQ(sort(g.nodes()), sort({1, 2}));
     std::vector<std::tuple<int, int, double>> expected_edges = {{1, 2, 5}};
     EXPECT_EQ(g.edges(), expected_edges);
 }
-
-// The rest are algorithms (Dijkstra, Prim, etc): the tests should be in this file; the algorithms should be functions in include/graph.hpp
