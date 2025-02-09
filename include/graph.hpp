87f3de7e776e8c4b2ac13a28d2fd4f7c5cbc2b85
Lucas Grenier <luc@sgrenier.com>
2025-02-09 12:05:56 -0500

    Make Graph less error-happy

--- include/graph.hpp
+++ include/graph.hpp
@@ -26,27 +26,27 @@ public:
         return _adjacency.size();
     }
     std::vector<T> nodes() { // not recommended to use
-        std::vector<T> ret;
+        std::vector<T> nodes;
         for (const auto key_value : _adjacency) {
-            ret.push_back(key_value.first);
+            nodes.push_back(key_value.first);
         }
-        return ret;
+        return nodes;
     }
     std::vector<std::tuple<T, T, double>> edges() { // not recommended to use
-        std::vector<std::tuple<T, T, double>> ret;
+        std::vector<std::tuple<T, T, double>> edges;
         for (const auto begin_ends : _adjacency) {
             for (const auto end_weight : begin_ends.second) {
-                ret.push_back({begin_ends.first, end_weight.first, end_weight.second});
+                edges.push_back({begin_ends.first, end_weight.first, end_weight.second});
             }
         }
-        return ret;
+        return edges;
     }
     void add_node(T node) {
         _adjacency[node] = {};
     }
     void set_edge(T begin, T end, double weight) {
-        if (!_adjacency.contains(begin)) throw NonexistentNode(begin);
-        if (!_adjacency.contains(end)) throw NonexistentNode(end);
+        if (!_adjacency.contains(begin)) add_node(begin);
+        if (!_adjacency.contains(end)) add_node(end);
         _adjacency[begin][end] = weight;
     }
     bool has_node(T node) {
@@ -59,16 +59,15 @@ public:
         return false;
     }
     void del_node(T node) {
-        if (!_adjacency.contains(node)) throw NonexistentNode(node);
+        if (!_adjacency.contains(node)) return;
         _adjacency.erase(node);
         for (auto i : _adjacency) {
             _adjacency[i.first].erase(node);
         }
     }
     void del_edge(T begin, T end) {
-        if (!_adjacency.contains(begin)) throw NonexistentNode(begin);
-        if (!_adjacency.contains(end)) throw NonexistentNode(end);
-        if (!_adjacency[begin].contains(end)) throw NonexistentEdge(begin, end);
+        if (!_adjacency.contains(begin)) return;
+        if (!_adjacency[begin].contains(end)) return;
         _adjacency[begin].erase(end);
     }
     double weight(T begin, T end) {
--- testing/graph.cpp
+++ testing/graph.cpp
@@ -2,28 +2,23 @@
 
 #include "graph.hpp"
 
-std::vector<int> sort(std::vector<int> arr) { // quick-and-dirty selection sort
-    for (int i = 0; i < arr.size(); ++i) {
-        for (int j = i; j < arr.size(); ++j) {
-            if (arr[j] < arr[i]) {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-            }
-        }
-    }
-    return arr;
-}
-
 TEST(GraphTest, InsertionAndDeletion) {
     Graph<int> g;
     g.add_node(1);
-    EXPECT_THROW(g.set_edge(1, 2, 5);, NonexistentNode);
+    EXPECT_THROW(g.weight(1, 2);, NonexistentNode);
     g.add_node(2);
     EXPECT_THROW(g.weight(1, 2);, NonexistentEdge);
     g.set_edge(1, 2, 5);
     EXPECT_DOUBLE_EQ(g.weight(1, 2), 5);
-    EXPECT_EQ(sort(g.nodes()), sort({1, 2}));
-    std::vector<std::tuple<int, int, double>> expected_edges = {{1, 2, 5}};
-    EXPECT_EQ(g.edges(), expected_edges);
+    g.set_edge(3, 4, 27.4); // shouldn't throw anything
+
+    std::vector<int> expected_nodes = { 1, 2, 3, 4 };
+    std::vector<int> actual_nodes = g.nodes();
+    std::sort(actual_nodes.begin(), actual_nodes.end());
+    EXPECT_EQ(expected_nodes, actual_nodes);
+
+    std::vector<std::tuple<int, int, double>> expected_edges = { { 1, 2, 5 }, { 3, 4, 27.4 } };
+    std::vector<std::tuple<int, int, double>> actual_edges = g.edges();
+    std::sort(actual_edges.begin(), actual_edges.end());
+    EXPECT_EQ(expected_edges, actual_edges);
 }
